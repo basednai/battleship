d092ae44645782fb147fa6c06b110f66509800bf
basednai <naiym.harris@icloud.com>
2024-10-29 22:29:32 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import "./styles.css";
-import "./getWeather";
-import "./weatherContainer";
 
-// Promise Implementation
-// if (!localStorage.getItem("weather")) {
-//   fetch(url, {
-//     method: "GET",
-//     headers: {},
-//   })
-//     .then((response) => {
-//       weather = response.json();
-//       localStorage.setItem("weather", JSON.stringify(weather));
-//       console.log(response.json());
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// } else {
-//   weather = JSON.parse(localStorage.getItem("weather"));
-// }
+
+document.querySelector("body").textContent = "Test Content"
+
+
+console.log("this is a test");
